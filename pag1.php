ae7ae1d0d2a8147ab540e9d0a61a7b9bc574cfc8
Ale <alejandraalvarado653@gmail.com>
2018-08-26 21:15:37 -0500

    Creacion de pagina php

--- pag1.php
+++ pag1.php
@@ -4,12 +4,15 @@
 <title> Pagina Web </title>
 </head>
 <body style="background-color:pink;">
- <?php 
-	$precio1=27.80;
-   echo '<h1 style="color:purple;" align="center">Bienvenido a Dulces momentos </h1>'; 
-	echo'<p align="center"> Conoce nuestros platillos</p>';
-	
-	echo'<table class="table table-striped" border="1" bordercolor="blue">';
+ <?php
+	$precio1=27.86;
+	$precio2=25.66;
+	$precio3=19.40;
+	$precio4=18.25;
+   echo '<h1 style="color:purple;" align="center">Bienvenido a Dulces momentos </h1>';
+	echo'<h3 align="center"> Conoce nuestros platillos</h3>';
+	echo '<img src= "705.jpg" align="right" hspace="5" vspace="5"/>';
+	echo'<table class="table table-striped" border="1" bordercolor="blue" align="left">';
 	echo'<thead>';
     echo'<tr>';
 	echo'<th> Pastel </th>';
@@ -22,10 +25,9 @@
 
 	$pasteles = array(
 	"Chocolate" => "$precio1",
-    "Vainilla" => "25.60",
-	"Marmoleado"=>"19.40",
-	"Tres leches"=>"18.00");
-	
+    "Vainilla" => "$precio2",
+	"Marmoleado"=>"$precio3",
+	"Tres leches"=>"$precio4");
 	while (list($clave, $valor) = each($pasteles)) {
     echo '<tr> <td>';
 	echo"$clave";
@@ -35,9 +37,9 @@
 	}
 	echo'</tbody>';
 	echo'</table>';
-    echo '<img src= "selva-negra.png"  width="150" height="150"/>';	
-	
-	echo'<table class="table table-striped" border="1" bordercolor="green">';
+    echo '<img src= "selva-negra.png"  width="150" height="150" align="center" />';
+	echo '<br> </br>';
+	echo'<table class="table table-striped" border="1" bordercolor="green" align="left">';
 	echo'<thead>';
     echo'<tr>';
 	echo'<th> Pays </th>';
@@ -49,10 +51,10 @@
 	echo'<tbody>';
 
 	$pays = array(
-	"Limon" => "28.80",
-    "Queso" => "25.60",
-	"Zarzamora"=>"19.40",
-	"Platano"=>"18.00");
+	"Limon" => "precio2",
+    "Queso" => "precio3",
+	"Zarzamora"=>"precio4",
+	"Platano"=>"precio1");
 	
 	while (list($clave, $valor) = each($pays)) {
     echo '<tr> <td>';
@@ -63,7 +65,7 @@
 	}
 	echo'</tbody>';
 	echo'</table>';
-	echo '<img src= "limon.png" width="150" height="150"/>';
+	echo '<img src= "limon.png" width="150" height="150" align="center"/>';
 	
 	
 	
